44a19afb92effa843343b6f2a30f3aa54768fab9
RoWe98 <wzx8551517@163.com>
2019-08-16 21:59:10 +0800

    Add sqstack

--- datastr/stack/sqstack.cpp
+++ datastr/stack/sqstack.cpp
@@ -15,6 +15,59 @@ typedef struct{
 
 bool InitStack(SqStack &S)
 {
-  S.base = new SElemType;
+  S.base = new int[STACK_INIT_SIZE];
+  if(!S.base) return false;
+  S.top = S.base;
+  S.stacksize = STACK_INIT_SIZE;
+  return true;
+}
 
+bool Push(SqStack &S, SElemType e)
+{
+  if(S.top-S.base>=S.stacksize)
+  {
+    return false;
+  }
+  if(!S.base) return false;
+  *S.top = e;
+  S.top ++;
+  return true;
+}
+
+bool Pop(SqStack &S, SElemType &e)
+{
+  if(S.top==S.base) return false;
+  --S.top;
+  e = *S.top;
+  std::cout << "pop element:"<<e<<std::endl;
+  return true;
+}
+
+void Output(SqStack S)
+{
+  int stack_length = S.top-S.base;
+  std::cout<<"stack_length:"<<stack_length<<std::endl;
+  while(stack_length!=0){
+    int e;
+    //std::cout<<"num:"<<e<<std::endl;
+    //S.top--;
+    if(Pop(S,e)) std::cout<<"success!"<<std::endl;
+    stack_length--;
+  }
+}
+
+int main()
+{
+  SqStack stack;
+  int nums;
+  if(InitStack(stack)) std::cout << "stack init Success!"<<std::endl;
+  for(int i=0;i<5;i++)
+  {
+    std::cout<<"input nums:";
+    std::cin>>nums;
+    if(Push(stack,nums))
+      std::cout<<"num added"<<std::endl;
+  }
+  Output(stack);
+  return 0;
 }
