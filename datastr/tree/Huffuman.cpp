948e51f0a0c5255fab6aed861a58d7eea7d304e3
RoWe98 <wzx8551517@163.com>
2019-09-19 23:00:33 +0800

    Update

--- datastr/tree/Huffuman.cpp
+++ datastr/tree/Huffuman.cpp
@@ -2,6 +2,7 @@
 #include "cstdio"
 #include "cstdlib"
 #include "malloc.h"
+#include "string.h"
 
 using namespace std;
 
@@ -10,82 +11,140 @@ typedef struct{
 	int parent,lch,rch;
 }HTNode,*HuffmanTree;
 
+typedef char **HuffmanCode;
 
-void Select(HuffmanTree &HT,int m,int &S1,int &S2)
+
+void Select(HuffmanTree &HT,int Count,int &s1,int &s2)
 {
-	int j;
-	S1=0;
-	S2=0;
-	for(j=1;j<=m;j++)
-	{
-		while(HT[j].parent==0)
-		{
-			if(HT[j].weight<HT[S1].weight)
-			{	
-				S1=j;
-				cout << S1 << endl;
-			}
-		}
-	}
+  unsigned int temp1=0;
+  unsigned int temp2=0;
+  unsigned int temp3;
+  for(int i=1;i<=Count;i++)
+  {
+    if(HT[i].parent==0)
+    {
+      if(temp1==0)
+      {
+        temp1=HT[i].weight;
+        (s1)=i;
+      }
+      else
+      {
+        if(temp2==0)
+        {
+          temp2=HT[i].weight;
+          (s2)=i;
+          if(temp2<temp1)
+          {
+            temp3=temp2;
+            temp2=temp1;
+            temp1=temp3;
+            temp3=(s2);
+            (s2)=(s1);
+            (s1)=temp3;
+          }
+        }
+        else
+        {
+          if(HT[i].weight<temp1)
+          {
+            temp2=temp1;
+            temp1=HT[i].weight;
+            (s2)=(s1);
+            (s1)=i;
+          }
+          if(HT[i].weight>temp1&&HT[i].weight<temp2)
+          {
+            temp2=HT[i].weight;
+            (s2)=i;
+          }
+        }
+      }
+    }
+  }
+}
 
-	for(j=1;j<=m;j++)
+void OutputTree(HuffmanTree HT,int n)
+{
+	cout << "HT" << "\tweight"<<"\tparent "<<"\tlchild "<<"\trchild"<<endl;
+	for(int i=1;i<=2*n-1;i++)
 	{
-		while(HT[j].parent==0&&j!=S1)
-		{
-			if(HT[j].weight<HT[S2].weight)
-			{	
-				S2=j;
-				cout << S2 << endl;
-			}
-		}
+		cout << i <<"\t"<<HT[i].weight<<"\t"<<HT[i].parent<<"\t";
+		cout << HT[i].lch <<"\t"<<HT[i].rch<<endl;
 	}
 }
 
 void CreateHuffmanTree(HuffmanTree &HT,int n,int &S1,int &S2)
 {
-	if(n<=1) 
-	{
-		cout << "leafs less than 2!" << endl;
-		return;
+    int m;
+    int i;
+    if(n<=1)
+    {
+		printf("叶子结点不足2个\n");
+    	return;
 	}
-	int i = 0;
-	int m=2*n-1;
-	HT = new HTNode[m+1];
-	for(i = 1;i<m;i++)
-	{
-		HT[i].lch=0;
-		HT[i].rch=0;
-		HT[i].parent=0;
-	}
-
-	for(i=1;i<=n;i++)
-	{
-		cout << "Please input the "<<i<<" leaf's weight:";
-		cin >> HT[i].weight;
-		cout << HT[i].weight<<endl;
-	}
-	cout<<1;
-	for(i=n+1;i<=m;i++)
-	{
-		cout<<1;
-		Select(HT,i-1,S1,S2);
-		HT[S1].parent=i;
-		HT[S2].parent=i;
-		HT[i].lch=S1;
-		HT[i].rch=S2;
-		HT[i].weight=HT[S1].weight+HT[S2].weight;
-	}
-	cout << "Huffman Tree build success!"<<endl;
+    m=2*n-1;
+    HT=new HTNode[m+1];
+    for(i=1;i<m;i++)
+    {
+        HT[i].parent=0;
+        HT[i].lch=0;
+        HT[i].rch=0;
+    }
+    for(i=1;i<=n;i++)
+    {
+        printf("请输入第%d个叶子结点的权值:",i);
+        scanf("%d",&HT[i].weight);
+    }
+    /*初始化结束*/
+    for(i=n+1;i<=m;i++)
+    {
+        Select(HT,i-1,S1,S2);
+        HT[S1].parent=i;
+        HT[S2].parent=i;
+        HT[i].lch=S1;
+        HT[i].rch=S2;
+        HT[i].weight=HT[S1].weight+HT[S2].weight;
+    }
+    printf("哈夫曼树创建完成\n");
 }
+
+void CreateHuffmanCode(HuffmanTree HT,HuffmanCode &HC,int n)
+{
+	int start,c,f;
+    char *cd = (char*)malloc(n*sizeof(char));
+	HC = new char*[n+1];
+	cd[n-1]='\0';
+	for(int i=1;i<n;++i)
+	{
+		start = n-1;
+		for(c=i,f=HT[i].parent;f!=0;c=f,f=HT[f].parent)
+		{
+			if(HT[f].lch==c) cd[--start]='0';
+			else cd[--start]='1';
+		}
+		HC[i]=(char*)malloc((n-start)*sizeof(char));
+		strcpy(HC[i],&cd[start]);
+	}
+	for(int i =0;i<n;i++)
+	{
+		cout << HC[i] <<endl;
+	}
+	free(cd);
+}
+
 
 int main()
 {
 	int S1,S2;
 	int n;
 	HuffmanTree HT;
+	HuffmanCode HC;
 	cout << "input the num of leaf:";
 	cin >> n;
 	CreateHuffmanTree(HT,n,S1,S2);
+	OutputTree(HT,n);
+	CreateHuffmanCode(HT,HC,n);
 	return 0;
 }
 
