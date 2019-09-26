37c9c34d8dd66003a9ce3f4a271ca12019b9cabe
root <root@localhost.localdomain>
2019-09-26 00:47:03 -0400

    Update

--- datastr/graph/ALGraph.cpp
+++ datastr/graph/ALGraph.cpp
@@ -27,16 +27,53 @@ typedef struct{
   int kind;
 }ALGraph;
 
+int LocateVex(ALGraph G,char vetex)
+{
+  int locate;
+  for(int i=0;i<G.vexnum;++i)
+  {
+    if(vetex==G.vertices[i].data)
+      locate = i;
+    else 
+      continue;
+  }
+  return locate;
+}
+
 bool Create(ALGraph &G)
 {
+  char V1,V2;
+  cout << "input the vexnum and arcnum:";
   cin >> G.vexnum >> G.arcnum;
   for(int i=0;i<G.vexnum;++i)
   {
+    cout << "input the data of vex:";
     cin >> G.vertices[i].data;
     G.vertices[i].firstarc=NULL;
+    cout << endl;
   }
   for(int k=0;k<G.arcnum;++k)
   {
+    cout << "input the info the vetex:";
     cin >> V1 >> V2;
+    int i = LocateVex(G,V1);
+    int j = LocateVex(G,V2);
+    ArcNode *p1 = new ArcNode;
+    p1->adjvex=j;
+    p1->nextarc = G.vertices[i].firstarc;
+    G.vertices[i].firstarc=p1;
+    cout << endl;
   }
+  return true;
 }
+
+
+
+int main()
+{
+  ALGraph G;
+  if(Create(G))
+    cout << "create graph success!" << endl;
+  return 0;
+}
+
